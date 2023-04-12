41bb0bbe7058912854e0a6da12ce43a51975ed53
hedystrong <hhedy7777@gmail.com>
2023-04-12 21:19:14 +0800

    category hiiw

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { SearchBox } from "./SearchBox";
 
-
-
-export const Search = () => {
-    return (
-        <div>Search</div>
-    )
-}
+export const Search = (props): JSX.Element => {
+  return (
+    <div>
+      <input
+        className="search-box"
+        type="search"
+        placeholder="search..."
+        onChange={props.handleChange}
+      />
+    </div>
+  );
+};
