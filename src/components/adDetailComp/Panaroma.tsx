8ea271c32c64d5b0578aee03a489b69b13afbb73
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-05-01 14:06:15 +0800

    20230501

--- src/components/adDetailComp/Panaroma.tsx
+++ src/components/adDetailComp/Panaroma.tsx
@@ -1,23 +1,35 @@
-import React, { useRef, useEffect } from "react";
-import PhotoSphereViewer from "photo-sphere-viewer";
-
-function PanoramaViewer() {
-  const container = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const viewer = new window.PhotoSphereViewer({
-      container: container.current!,
-      panorama: "path/to/pano.jpg",
-      default_position: { long: (-90 * Math.PI) / 180, lat: 0 },
-      mousewheel: false,
-    });
-
-    return () => {
-      viewer.destroy();
-    };
-  }, []);
-
-  return <div id="pano-container" ref={container}></div>;
+import { Pannellum } from "pannellum-react";
+import * as React from 'react';
+function ImagePreview = () => {
+  const [yaw, setYaw] = React.useState(0);
+  const [pitch, setPitch] = React.useState(0);
+  const panImage: any = React.useRef(null);
+  return (
+    <>
+      <div> Pitch: {pitch} </div>
+      <div> Yaw: {yaw} </div>
+      <Pannellum
+        width="100%"
+        height="500px"
+        image="https://i2.wp.com/www.samrohn.com/wp-content/uploads/le-meridien-bedroom-panorama.jpg?resize=1200%2C600"
+        pitch={10}
+        yaw={180}
+        hfov={110}
+        autoLoad
+        showZoomCtrl={false}
+        onMouseup={(event) => {
+          setPitch(panImage.current.getViewer().mouseEventToCoords(event)[0]);
+          setYaw(panImage.current.getViewer().mouseEventToCoords(event)[1]);
+        }}
+      >
+        <Pannellum.Hotspot
+          type="custom"
+          pitch={12.41}
+          yaw={117.76}
+          handleClick={(evt, name) => console.log(name)}
+          name="image info"
+        />
+      </Pannellum>
+    </>
+  )
 }
-
-export default PanoramaViewer;
