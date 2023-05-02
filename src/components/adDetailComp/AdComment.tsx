8098f65b4a5778b8fc5a4ba5872524220e64613b
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-05-02 13:45:39 +0800

    20230502 nado

--- src/components/adDetailComp/AdComment.tsx
+++ src/components/adDetailComp/AdComment.tsx
@@ -2,15 +2,23 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { CommentCard } from "./CommentCard";
 import { AddCommentSection } from "./AddCommentSection";
+import jwt from "jsonwebtoken";
 
 export const AdComment = ({ data }: any) => {
   const [commentData, setCommentData] = useState<Array<any>>([]);
   const [loading, setLoading] = useState(true);
-  const [newComment, setNewComment] = useState(true);
+  const [decoded, setDecoded] = useState<object | string>();
+  const [commentBody, setCommentBody] = useState<object>({
+    propertyID: "",
+    userID: "",
+    comment: [],
+  });
+  const [token, setToken] = useState<string>();
+
 
   useEffect(() => {
     getCommentData(data?.propertyID?._id);
-  }, [data, newComment]);
+  }, [data]);
 
   function getCommentData(id: any) {
     if (id) {
@@ -29,12 +37,42 @@ export const AdComment = ({ data }: any) => {
     }
   }
 
+
+
+  useEffect(() => {
+    let localStorageValue: string = localStorage.getItem("token") || "";
+    setDecoded(jwt.decode(localStorageValue) || "");
+    if (localStorageValue.length > 1) {
+      setToken(localStorageValue);
+    }
+  }, []);
+
+  function postComment(e: any) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:9000/api/procomment", commentBody, {
+        headers: {
+          "x-access-token": token,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        commentBody.comment[0] = "";
+        getCommentData(data?.propertyID?._id)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   if (loading) {
     return (
       <AddCommentSection
         propertyId={data?.propertyID?._id}
-        setNewComment={setNewComment}
-        newComment={newComment}
+        setCommentBody={setCommentBody}
+        decoded={decoded}
+        postComment={postComment}
+        commentBody={commentBody}
       />
     );
   } else {
@@ -42,8 +80,10 @@ export const AdComment = ({ data }: any) => {
       <div>
         <AddCommentSection
           propertyId={data?.propertyID?._id}
-          setNewComment={setNewComment}
-          newComment={newComment}
+          setCommentBody={setCommentBody}
+          decoded={decoded}
+          postComment={postComment}
+          commentBody={commentBody}
         />
         {commentData?.map((e, index): any => {
           return (
