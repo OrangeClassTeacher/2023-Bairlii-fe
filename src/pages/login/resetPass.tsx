802c67dd2608c0dcac031d362899d99fed1283cf
hedystrong <hhedy7777@gmail.com>
2023-05-09 00:11:47 +0800

    0509 byambaa FE resetPass done

---
+++ src/pages/login/resetPass.tsx
@@ -0,0 +1,9 @@
+import ResetPassword from "@/components/user/ResetPassword";
+
+export default function ResetPassword1(): JSX.Element {
+  return (
+    <div>
+      <ResetPassword />
+    </div>
+  );
+}
