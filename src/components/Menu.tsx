866a2803443dee9d679dce4409ed2d1c57d60985
Uuganbayr Bymbajaw <byambajawbebe@Uuganbayrs-MacBook-Pro.local>
2023-05-09 15:32:53 +0800

    fix user page

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -6,7 +6,6 @@ import useAllModal from "@/hooks/useAllModal";
 
 const Menu = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
-  const rentModal = useAllModal();
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
@@ -14,12 +13,6 @@ const Menu = (): JSX.Element => {
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
-        <div
-          className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
-          onClick={rentModal.isOpen ? rentModal.onClose : rentModal.onOpen}
-        >
-          Your Home
-        </div>
         <div
           onClick={toggleOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition"
