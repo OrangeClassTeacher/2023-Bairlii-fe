90c09933d61fd3bd00ad9e33a8698e94329c470d
Uuganbayr Bymbajaw <byambajawbebe@Uuganbayrs-MacBook-Pro.local>
2023-05-18 12:25:43 +0800

    fix my advertisement

--- src/components/AddProperty/AdCardForUser.tsx
+++ src/components/AddProperty/AdCardForUser.tsx
@@ -6,35 +6,59 @@ const AdCardForUser = ({ item, key, deleteAdvertisement }: any) => {
   const date = new Date(item?.createdAt);
 
   return (
-    <div
-      className="flex flex-wrap flex-col max-w-[10px] min-w-[300px]"
-      key={key}
-    >
-      <SwiperSlider data={item} />
-      <div></div>
-
-      <div className="rounded-2xl bg-slate-300/90 flex flex-col w-[85%] p-6 -mt-2 ml-2 z-20">
-        <span className="flex">
-          Area:
-          <div className="flex self-end">{item?.propertyID?.area}.m.k</div>
-        </span>
-        <span>Room Number: {item?.propertyID?.roomNumber}</span>
-        <span>Location:{item?.propertyID?.locationName}</span>
-        <span>Price: {item?.price}</span>
-        <span>
-          From: {date.getFullYear()} {date.getMonth() + 1} {date.getDate()}
-        </span>
-        <Link
-          href={{
-            pathname: `/[detail]`,
-            query: {
-              detail: item?._id,
-            },
-          }}
+    <div className="card">
+      <div className="card-inner ">
+        <div
+          className="  flex flex-wrap flex-col max-w-[10px] min-w-[300px] "
+          key={key}
         >
-          See More ...
-        </Link>
-        <button onClick={() => deleteAdvertisement(item?._id)} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delete</button>
+          <SwiperSlider data={item} />
+          <div className=" bg-gray-100  z-20 flex flex-wrap justify-center ">
+            <div className="flex justify-between w-4/5 mt-2  ">
+              <p className="font-bold">Area:</p>
+              <p>{item?.propertyID?.area}.m.k</p>
+            </div>
+            <div className="flex justify-between w-4/5 mt-2 ">
+              <p className="font-bold">Room Number:</p>
+              <p>{item?.propertyID?.roomNumber}</p>
+            </div>
+            <div className="flex justify-between w-4/5 mt-2 ">
+              <p className="font-bold">Location:</p>
+              <p>{item?.propertyID?.locationName}</p>
+            </div>
+            <div className="flex justify-between w-4/5 mt-2 ">
+              <p className="font-bold">Price:</p>
+              <p>{item?.price}</p>
+            </div>
+            <div className="flex justify-between w-4/5 mt-2">
+              <p className="font-bold"> From:</p>{" "}
+              <p>
+                {date.getFullYear()} {date.getMonth() + 1} {date.getDate()}
+              </p>
+            </div>
+            <div className="w-4/5 mt-4 flex flex-row-reverse text-gray-600 text-sm">
+              <Link
+                href={{
+                  pathname: `/[detail]`,
+                  query: {
+                    detail: item?._id,
+                  },
+                }}
+              >
+                See More ...
+              </Link>
+            </div>
+            <div>
+              <button
+                onClick={() => deleteAdvertisement(item?._id)}
+                type="button"
+                className="mt-4 text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/AddProperty/Ads.tsx
+++ src/components/AddProperty/Ads.tsx
@@ -27,13 +27,13 @@ const Ads = ({ setSelected }: any): JSX.Element => {
     getData();
   }, [curPageNumb, params]);
 
-
-
-
   function getData() {
     setLoading(true);
     axios
-      .post("http://localhost:9000/api/advertisements", { ...reqBody, ...currentQuery })
+      .post("http://localhost:9000/api/advertisements", {
+        ...reqBody,
+        ...currentQuery,
+      })
       .then((res) => {
         setAds(res.data.result.reverse());
         setPageNumb(Math.ceil(res.data.rowCount / 12));
@@ -44,8 +44,6 @@ const Ads = ({ setSelected }: any): JSX.Element => {
       });
   }
 
-
-
   if (pageNumb) {
     for (
       let i: number = curPageNumb - 10;
@@ -55,8 +53,6 @@ const Ads = ({ setSelected }: any): JSX.Element => {
       pages.push(i);
     }
   }
-
-
 
   return (
     <div className="flex flex-wrap gap-6 justify-center mt-7 max-w-7xl w-full">
@@ -88,7 +84,7 @@ const Ads = ({ setSelected }: any): JSX.Element => {
         </div>
       ) : (
         <div>
-          <div className="flex flex-wrap gap-6 justify-center mt-7 max-w-7xl w-full">
+          <div className="flex flex-wrap gap-6 justife-between mt-7 max-w-7xl w-full">
             {ads?.map((item, index): JSX.Element => {
               return (
                 <>
