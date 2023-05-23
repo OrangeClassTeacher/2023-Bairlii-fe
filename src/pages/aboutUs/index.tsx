9af0845e8868f0027044a5c5e503b2741f15b9c4
hedystrong <hhedy7777@gmail.com>
2023-05-24 00:18:34 +0800

    about us

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -1,127 +1,73 @@
 import Image from "next/image";
 
 function AboutUs(): JSX.Element {
-  const about = [
-    {
-      title: [
-        { id: 1, text: "Nado" },
-        { id: 2, text: "Bebe" },
-        { id: 3, text: "Byambaa" },
-      ],
-    },
-    {
-      description: [
-        { id: 1, text: "Welcome to " },
-        { id: 2, text: "sdkjfsldjf" },
-        { id: 3, text: "iorjeoifj" },
-      ],
-    },
-  ];
-
   return (
     <>
-      <section className="section">
-        <div className="section__container">
-          <div className="content">
-            <p className="subtitle">HELLO</p>
-            <h1 className="title">
-              I'm
-              <span>
-                Narandorj
-                <br />a
-              </span>
-              Web Developer
-            </h1>
-            <p className="description">
-              Welcome to my web developer portfolio! I'm Narandorj, a skilled
-              and creative web developer with a passion for creating beautiful,
-              responsive, and user-friendly websites. I've worked on a variety
-              of web projects, ranging from personal blogs to e-commerce
-              platforms.
-            </p>
-            <div className="action__btns">
-              <button className="hire__me">Hire Me</button>
-              <button className="portfolio">Portfolio</button>
+      <div className="flex max-w-full mx-auto gap-8">
+        <section id="section">
+          <div className="aboutUsCard">
+            <div id="imgBox">
+              <Image
+                id="aboutImg"
+                src="/images/nado.jpg"
+                width={300}
+                height={200}
+                alt="aboutUs"
+              />
+            </div>
+            <div id="content">
+              <h2 className="text-black-700">Hello I`m Narandorj</h2>
+              <p className="mt-5">
+                Welcome to my web developer portfolio! I'm Narandorj, a skilled
+                and creative web developer with a passion for creating
+                beautiful, responsive, and user-friendly websites.
+              </p>
             </div>
           </div>
-          <div className="image">
-            <Image
-              src="/images/nado.jpg "
-              width={500}
-              height={500}
-              alt="profile"
-            />
-          </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="section__container">
-          <div className="image">
-            <Image
-              src="/images/bebe.jpg "
-              width={500}
-              height={500}
-              alt="profile"
-            />
-          </div>
-          <div className="content">
-            <p className="subtitle">HELLO</p>
-            <h1 className="title">
-              I'm
-              <span>
-                Byambajav
-                <br />a
-              </span>
-              Web Developer
-            </h1>
-            <p className="description">
-              Welcome to my web developer portfolio! I'm Byambajav, a skilled
-              and creative web developer with a passion for creating beautiful,
-              responsive, and user-friendly websites. I've worked on a variety
-              of web projects, ranging from personal blogs to e-commerce
-              platforms.
-            </p>
-            <div className="action__btns">
-              <button className="hire__me">Hire Me</button>
-              <button className="portfolio">Portfolio</button>
+        </section>
+        <section id="section">
+          <div className="aboutUsCard">
+            <div id="imgBox">
+              <Image
+                id="aboutImg"
+                src="/images/z2.jpg"
+                width={300}
+                height={200}
+                alt="aboutUs"
+              />
+            </div>
+            <div id="content">
+              <h2>Hello I`m Byambasuren</h2>
+              <p className="mt-5">
+                Welcome to my web developer portfolio! I'm Byambasuren, a
+                skilled and creative web developer with a passion for creating
+                beautiful, responsive, and user-friendly websites.
+              </p>
             </div>
           </div>
-        </div>
-      </section>
-      <section className="section">
-        <div className="section__container">
-          <div className="content">
-            <p className="subtitle">HELLO</p>
-            <h1 className="title">
-              I'm
-              <span>
-                Byambasuren
-                <br />a
-              </span>
-              Web Developer
-            </h1>
-            <p className="description">
-              Welcome to my web developer portfolio! I'm Byambasuren, a skilled
-              and creative web developer with a passion for creating beautiful,
-              responsive, and user-friendly websites. I've worked on a variety
-              of web projects, ranging from personal blogs to e-commerce
-              platforms.
-            </p>
-            <div className="action__btns">
-              <button className="hire__me">Hire Me</button>
-              <button className="portfolio">Portfolio</button>
+        </section>
+        <section id="section">
+          <div className="aboutUsCard">
+            <div id="imgBox">
+              <Image
+                id="aboutImg"
+                src="/images/bebe.jpg"
+                width={300}
+                height={200}
+                alt="aboutUs"
+              />
+            </div>
+            <div id="content">
+              <h2>Hello I`m Byambajav</h2>
+              <p className="mt-5">
+                Welcome to my web developer portfolio! I'm Byambajav, a skilled
+                and creative web developer with a passion for creating
+                beautiful, responsive, and user-friendly websites.
+              </p>
             </div>
           </div>
-          <div className="image">
-            <Image
-              src="/images/byamba.jpg "
-              width={500}
-              height={500}
-              alt="profile"
-            />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
